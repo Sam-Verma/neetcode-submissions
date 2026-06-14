6eacd86d99069fab20083f9215b1ccdc4cdf89fc
Shivam Krishna <84848637+Sam-Verma@users.noreply.github.com>
2026-06-14 18:39:04 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        if len(s)<len(t):
+            return ""
+        
+        countT = collections.Counter(t)
+        window = defaultdict()
+
+        idx = [-1,-1]
+        resLen = float("infinity")
+        have,need = 0,len(countT)
+
+        l=0
+        for r in range(len(s)):
+            
+            c = s[r]
+            window[c] = 1 + window.get(c,0)
+
+            if c in countT and window[c]==countT[c]:
+                have+=1
+            
+            while have==need:
+                if (r-l+1)<resLen:
+                    idx = [l,r]
+                    resLen = r-l+1
+                
+                window[s[l]]-=1
+                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                    have-=1
+                l+=1
+        
+        l,r=idx
+        return s[l:r+1] if resLen!=float('infinity') else ''
